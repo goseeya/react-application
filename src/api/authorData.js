8ef9ef1fe2ae08318a12c1ebd84d940b2a4af576
Rakowska <Malgorzata.Rakowska@ge.com>
2017-08-21 14:01:06 +0200

    authors section

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'malgorzata-rakowska', 
+			firstName: 'Malgorzata', 
+			lastName: 'Rakowska'
+		},	
+		{
+			id: 'pawel-nowak', 
+			firstName: 'Pawel', 
+			lastName: 'Nowak'
+		},	
+		{
+			id: 'adam-drabina', 
+			firstName: 'Adam', 
+			lastName: 'Drabina'
+		}
+	]
+};
