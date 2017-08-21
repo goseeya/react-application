da6758550a3e49bdb3197955df667d28e62269c6
Rakowska <Malgorzata.Rakowska@ge.com>
2017-08-21 11:42:41 +0200

    react

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homepage');
 
 
-var App = console.log('Hello world from Browserify');
-
-module.exports = App;
+React.render(<Home />, document.getElementById('app'));
