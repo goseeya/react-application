f21af90fe0a5361542cec52e6f6ee81ff9214a5e
Rakowska <Malgorzata.Rakowska@ge.com>
2017-08-21 13:17:47 +0200

    logo

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var config = {
     paths: {
         html: './src/*.html', //go to the src and find anything .html
         js: './src/**/*.js',
+        images: './src/images/*', //any images from folder
         css : [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -62,15 +63,27 @@ gulp.task('css', function() {
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+//Migrates images to dist folder
+//Note that I could even otpimize my images here
+gulp.task('images', function () {
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(connect.reload());
+
+    //publish favicon
+    gulp.src('./src/cat.ico')
+        .pipe(gulp.dest(config.paths.dist));
+});
+
 gulp.task('lint', function() {
     return gulp.src(config.paths.js)
         .pipe(lint({config: 'eslint.config.json'}))
         .pipe(lint.format());
-})
+});
 
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']); //anything sth changes, run html task
     gulp.watch(config.paths.js, ['js', 'lint']); 
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']); //each time i run gulp command 
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']); //each time i run gulp command 
